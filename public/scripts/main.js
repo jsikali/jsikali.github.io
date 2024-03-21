d85909a512023af81aba55ecd33d7867376e8c3c
jsikali <120147133+jsikali@users.noreply.github.com>
2024-03-21 04:54:30 -0400

    its just the highlighting and images and i give up holy fucking shit where the fuck how do you do it fuck

---
+++ public/scripts/main.js
@@ -0,0 +1,24 @@
+/* namespace. */
+var rhit = rhit || {};
+
+rhit.highlightText = function()
+{
+
+}
+
+rhit.main = function()
+{
+    let clickables = document.getElementsByClassName("clickable");
+    for(let i = 0; i < clickables.length; i++)
+    {
+        const clickable = clickables[i];
+
+        clickable.onmouseover = (event) =>
+        {
+            //how do you just change the color what  the fuck oh my fucking GOD
+            
+        }
+    }
+}
+
+rhit.main();
