2610aeccb28a7806080f9413dc549569575c2ca9
jsikali <120147133+jsikali@users.noreply.github.com>
2024-03-22 21:08:41 -0400

    ITS ALIGNE DUFCKING FINALLY MAN OH MY GOD

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,22 @@
 /* namespace. */
 var rhit = rhit || {};
 
-rhit.highlightText = function()
-{
+darkmode = true;
 
+
+rhit.colorMode = function()
+{
+    alert("hiiiiiiiiiiiiiii change colors when i get this in place");
+    if(darkMode)
+    {
+        background = "#23202d";
+        text = "#ebeaef";
+        header = "#17151e";
+        accent = "#fee285";
+    }else //light mode
+    {
+
+    }
 }
 
 rhit.main = function()
@@ -11,13 +24,37 @@ rhit.main = function()
     let clickables = document.getElementsByClassName("clickable");
     for(let i = 0; i < clickables.length; i++)
     {
-        const clickable = clickables[i];
+        const temp = clickables[i]; //this had a better name but didnt work. now it works with a shit name. ._.
 
-        clickable.onmouseover = (event) =>
+        temp.style.color = "#0dee7";
+        temp.onmouseover = (event) =>
         {
             //how do you just change the color what  the fuck oh my fucking GOD
-            
+            temp.style.color = "#f2a918";
         }
+
+        temp.onmouseleave = (event) =>
+        {
+            temp.style.color = "#ebeaef";
+        }
+
+        temp.onclick = (event) =>
+        {
+            temp.style.color = "#e0dee7";
+        }
+    }
+
+    let mode = document.getElementById("colorSwitch");
+
+    mode.onmouseover = (event) =>
+    {
+        // alert("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+    
+    mode.onclick = (event) =>
+    {
+        darkMode = !darkMode;
+        this.colorMode();
     }
 }
 
